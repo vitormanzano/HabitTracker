59dcd87c12bf385e2eb13aa9868cfe48b1879d7f
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-19 21:46:05 -0300

    feat: Initiate Habit class and Category class

---
+++ HabitTracker/src/HabitTracker.Habits.Domain/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HabitTracker.Core.DomainObjects;
+
+namespace HabitTracker.Habits.Domain
+{
+    public class Category : Entity
+    {
+        public string Name { get; private set; }
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- HabitTracker/src/HabitTracker.Habits.Domain/Habits/Habit.cs
+++ HabitTracker/src/HabitTracker.Habits.Domain/Habits/Habit.cs
@@ -4,6 +4,34 @@ namespace HabitTracker.Habits.Domain.Habits
 {
     public class Habit : Entity, IAggregateRoot
     {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int Frequency { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public Category Category { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public Habit(string title, string description, int frequency, Guid categoryId)
+        {
+            Title = title;
+            Description = description;
+            Frequency = frequency;
+            CategoryId = categoryId;
+            IsActive = true;
+        }
+
+        public void Deactivate() => IsActive = false;
+
+        public void Activate() => IsActive = true;
         
+        public void UpdateCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void UpdateDescription(string description) => Description = description;
+
+        public void DidHabit() => Frequency++;
     }
 }
