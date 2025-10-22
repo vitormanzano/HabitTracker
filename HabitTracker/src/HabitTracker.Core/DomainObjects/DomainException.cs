277ac6560b4b0c10f1d2c66d3116a45e7b215840
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-22 20:44:55 -0300

    feat: Adding DomainException

---
+++ HabitTracker/src/HabitTracker.Core/DomainObjects/DomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace HabitTracker.Core.DomainObjects
+{
+    public class DomainException : Exception
+    {
+        public DomainException() { }
+
+        public DomainException(string message) : base(message) { }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
