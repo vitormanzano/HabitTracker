8152f03e2bc1355ed634e3cbf11961ec38223c18
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-03-03 19:59:03 -0300

    fix: Fixing the tests because they are using a old constructor for the Habit

--- HabitTracker/tests/HabitTracker.Habits.Domain.Tests/HabitsTests.cs
+++ HabitTracker/tests/HabitTracker.Habits.Domain.Tests/HabitsTests.cs
@@ -9,42 +9,37 @@ namespace HabitTracker.Habits.Domain.Tests
         public void Habit_Validate_ValidationsShouldReturnExceptions()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Habit(string.Empty, "Description", 2, Guid.NewGuid()));
+                new Habit(string.Empty, "Description", Guid.NewGuid()));
             
             Assert.Equal("The title of the habit cannot be empty.", ex.Message);
 
             ex = Assert.Throws<DomainException>(() =>
-                new Habit("a", "Description", 2, Guid.NewGuid()));
+                new Habit("a", "Description", Guid.NewGuid()));
 
             Assert.Equal("The title of the habit must be between 3 and 100 characters.", ex.Message);
 
             ex = Assert.Throws<DomainException>(() =>
-                new Habit(new string('A', 101), string.Empty, 2, Guid.NewGuid()));
+                new Habit(new string('A', 101), string.Empty, Guid.NewGuid()));
 
             Assert.Equal("The title of the habit must be between 3 and 100 characters.", ex.Message);
 
             ex = Assert.Throws<DomainException>(() =>
-                new Habit("new Habit", string.Empty, 2, Guid.NewGuid()));
+                new Habit("new Habit", string.Empty, Guid.NewGuid()));
 
             Assert.Equal("The description of the habit cannot be empty.", ex.Message);
 
             ex = Assert.Throws<DomainException>(() =>
-                new Habit("new Habit", "abc", 2, Guid.NewGuid()));
+                new Habit("new Habit", "abc", Guid.NewGuid()));
 
             Assert.Equal("The description of the habit must be between 5 and 500 characters.", ex.Message);
 
             ex = Assert.Throws<DomainException>(() =>
-                new Habit("new Habit", new string('A', 501), 2, Guid.NewGuid()));
+                new Habit("new Habit", new string('A', 501), Guid.NewGuid()));
 
             Assert.Equal("The description of the habit must be between 5 and 500 characters.", ex.Message);
 
             ex = Assert.Throws<DomainException>(() =>
-                new Habit("new Habit", "Description", -1, Guid.NewGuid()));
-
-            Assert.Equal("The frequency of the habit must be a non-negative integer.", ex.Message);
-
-            ex = Assert.Throws<DomainException>(() =>
-                new Habit("new Habit", "Description", 2, Guid.Empty));
+                new Habit("new Habit", "Description", Guid.Empty));
 
             Assert.Equal("The category ID of the habit must be a valid GUID.", ex.Message);
         }
