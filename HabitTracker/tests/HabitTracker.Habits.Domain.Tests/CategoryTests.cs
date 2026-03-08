267e37767151acf944021aa3dd33d4ed3cba320d
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-03-08 20:13:19 -0300

    test: Adding category tests

---
+++ HabitTracker/tests/HabitTracker.Habits.Domain.Tests/CategoryTests.cs
@@ -0,0 +1,20 @@
+﻿using HabitTracker.Core.DomainObjects;
+
+namespace HabitTracker.Habits.Domain.Tests
+{
+    public class CategoryTests
+    {
+        [Fact]
+        public void Test_Validate_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainException>(() => new Category(string.Empty));
+            Assert.Equal("The name of the category cannot be empty.", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() => new Category("aa"));
+            Assert.Equal("The name of the category must be between 3 and 100 characters.", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() => new Category(new string('A', 101)));
+            Assert.Equal("The name of the category must be between 3 and 100 characters.", ex.Message);
+        }
+    }
+}
