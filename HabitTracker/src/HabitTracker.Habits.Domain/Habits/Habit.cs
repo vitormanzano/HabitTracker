b7fcc41db53569c8acac49cb8262bd97aef7cec7
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-03-10 18:32:21 -0300

    refactor: Adding methods to update title, description. Creating private methods to validate, using the AssertionConcern, but separating in other methods the old method Validate

--- HabitTracker/src/HabitTracker.Habits.Domain/Habits/Habit.cs
+++ HabitTracker/src/HabitTracker.Habits.Domain/Habits/Habit.cs
@@ -27,25 +27,51 @@ namespace HabitTracker.Habits.Domain.Habits
         public void Deactivate() => IsActive = false;
 
         public void Activate() => IsActive = true;
-        
+
+        public void UpdateTitle(string title)
+        {
+            ValidateTitle(title);
+            Title = title;
+        }
+
+        private void ValidateTitle(string title)
+        {
+            AssertionConcern.ValidateIfEmpty(Title, "The title of the habit cannot be empty.");
+            AssertionConcern.ValidateCharacters(Title, 3, 100, "The title of the habit must be between 3 and 100 characters.");
+        }
+
+        public void UpdateDescription(string description)
+        {
+            ValidateDescription(description);
+            Description = description;
+        }
+
+        private void ValidateDescription(string description)
+        {
+            AssertionConcern.ValidateIfEmpty(Description, "The description of the habit cannot be empty.");
+            AssertionConcern.ValidateCharacters(Description, 5, 500, "The description of the habit must be between 5 and 500 characters.");
+        }
+
         public void UpdateCategory(Category category)
         {
+            ValidateCategoryId(category.Id);
             Category = category;
             CategoryId = category.Id;
         }
 
-        public void UpdateDescription(string description) => Description = description;
+        private void ValidateCategoryId(Guid categoryId)
+        {
+            AssertionConcern.ValidateIfEquals(CategoryId, Guid.Empty, "The category ID of the habit must be a valid GUID.");
+        }
 
         public void DidHabit() => TimesCompleted++;
         public void UndidHabit() => TimesCompleted = TimesCompleted > 0 ? TimesCompleted - 1 : 0;
+
         public void Validate()
         {
-            AssertionConcern.ValidateIfEmpty(Title, "The title of the habit cannot be empty.");
-            AssertionConcern.ValidateCharacters(Title, 3, 100, "The title of the habit must be between 3 and 100 characters.");
-            AssertionConcern.ValidateIfEmpty(Description, "The description of the habit cannot be empty.");
-            AssertionConcern.ValidateCharacters(Description, 5, 500, "The description of the habit must be between 5 and 500 characters.");
-            AssertionConcern.ValidateMinimumMaximum(TimesCompleted, 0, int.MaxValue, "The frequency of the habit must be a non-negative integer.");
-            AssertionConcern.ValidateIfEquals(CategoryId, Guid.Empty, "The category ID of the habit must be a valid GUID.");
+            ValidateTitle(Title);
+            ValidateDescription(Description);
+            AssertionConcern.ValidateMinimumMaximum(TimesCompleted, 0, int.MaxValue, "The times completed of the habit must be a non-negative integer.");
         }
     }
 }
